daf598024295a3d58b4683db4337b8df4ad5f38e
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2021-11-09 16:03:41 -0300

    iniciando a reprograma music

---
+++ api-com-mongo-para-o-lar/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+
+const db = require("./database/mongoConfig")
+const musicRouter = require("./routes/musicRouter")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json)
+app.use("/music", musicRouter)
+
+db.connect()
+
+module.exports = app
---
+++ api-com-mongo-para-o-lar/src/database/mongoConfig.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+require("dotenv-safe").config()
+const MONGODB_URI = process.env.MONGODB_URI
+
+const connect = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("Banco conectado!");
+    } catch (error) {
+        console.log(error, message);
+    }
+}
+
+module.exports = { 
+    connect 
+}
